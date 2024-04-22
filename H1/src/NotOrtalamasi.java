41f13f626620770e8e84f05ca606ef12c32824f2
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-22 16:27:23 +0300

    Refactoring

---
+++ H1/src/NotOrtalamasi.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+
+        int mat, turkce, kimya, fizik, muzik, tarih;
+
+        Scanner calc = new Scanner(System.in);
+
+        System.out.println("Matematik notu:");
+        mat = calc.nextInt();
+
+        System.out.println("Türkçe notu:");
+        turkce = calc.nextInt();
+
+        System.out.println("Kimya notu:");
+        kimya = calc.nextInt();
+
+        System.out.println("Fizik notu:");
+        fizik = calc.nextInt();
+
+        System.out.println("Müzik notu:");
+        muzik = calc.nextInt();
+
+        System.out.println("Tarih notu:");
+        tarih = calc.nextInt();
+
+        int total = (mat+turkce+kimya+fizik+muzik+tarih);
+        double result = (total / 6.0);
+        System.out.println(result);
+    }
+}
