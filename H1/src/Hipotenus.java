5098381548bf28a9ccad5eff33d9d4cbb16ab6e6
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-22 17:47:16 +0300

    W1E3 Hioptenus hesaplayıcı eklendi.
    Patika W1 - Hipotenüs

---
+++ H1/src/Hipotenus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Hipotenus {
+    public static void main(String[] args) {
+        //Değişken atama ve kullanıcı girişi:
+      double a,b,c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("İlk girin kenarı girin: ");
+        a = input.nextDouble();
+
+        System.out.print("İkinci kenarı girin: ");
+        b = input.nextDouble();
+
+        System.out.print("Son kenarı girin: ");
+        c = input.nextDouble();
+
+        //Heron Formülü:
+        double u = (a+b+c) / 2;
+        double alan = u * (u-a) * (u-b) * (u-c);
+        alan = Math.sqrt(alan);
+        System.out.println("Üçgenin alanı: " + alan);
+    }
+}
