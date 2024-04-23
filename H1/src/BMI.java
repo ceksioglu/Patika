ccbe16806d1912e358a2bfea1b30b1c3267e9c72
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-23 18:03:09 +0300

    BMI ve ManavKasa eklendi.
    Patika W1

---
+++ H1/src/BMI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        //Kullanıcı girişi:
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Vücüt kütlesi(kg): ");
+         double kg = scanner.nextDouble();
+
+        System.out.println("Boy(cm): ");
+         double boy = scanner.nextDouble();
+         boy = boy / 100; //cm -> m
+
+         //BMI Hesaplama:
+        double BMI = kg / Math.pow(boy,2);
+        System.out.println("Vücüt kütle indeksiniz: " +BMI);
+
+        scanner.close(); //Memory leak
+    }
+}
