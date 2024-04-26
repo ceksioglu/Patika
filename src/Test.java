88b3d253f0ecb0301db30a7455801fedf2558977
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-26 22:56:15 +0300

    CiftSayi added.

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Yıl Giriniz: ");
-        int year = input.nextInt();
+        for (int k = 0; k < 9; k++) {
 
-        //formulating leap year.
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println(year + " - Bu bir artık yıldır!");
-        } else {
-            //if it's not a leap year, this is the another text option
-            System.out.println(year + " Bu bir artık yıldır değildir!");
+            System.out.print("*");
+
+            if (k == 3)
+
+                break;
+
         }
     }
-}
+    }
+
