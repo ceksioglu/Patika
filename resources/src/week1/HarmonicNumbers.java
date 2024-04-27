b5449889b45fa19682521e10aaa5df14c4f20df0
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-27 22:54:41 +0300

    Create HarmonicN.java

---
+++ resources/src/week1/HarmonicNumbers.java
@@ -0,0 +1,20 @@
+package week1;
+
+import java.util.Scanner;
+
+public class HarmonicNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number:");
+        int num = scanner.nextInt();
+        double result = 0;
+
+        for(double i = 1; i <= num; i++){
+            result += (1/i);
+        }
+        System.out.println("Result is: " +result);
+    }
+}
+//Can Ekşioğlu
