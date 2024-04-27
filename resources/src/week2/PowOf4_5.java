def948fcf74c7ccb4a20575381cfc7ed85866e2d
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-27 19:31:45 +0300

    Create Powof4_5.java

---
+++ resources/src/week2/PowOf4_5.java
@@ -0,0 +1,22 @@
+package week2;
+
+import java.util.Scanner;
+
+public class PowOf4_5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen sayıyı giriniz:");
+        int num = scanner.nextInt();
+
+        System.out.println("4'ün kuvvetleri:");
+        for(int i=1; i<=num; i*=4) {
+            System.out.println(i);
+        }
+        System.out.println("5'in kuvvetleri:");
+        for(int j=1; j<=num; j*=5) {
+            System.out.println(j);
+        }
+    }
+}
