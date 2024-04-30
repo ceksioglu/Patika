1d1ebc515640f5a686dd26b45b68cb761d8645e7
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-01 01:01:15 +0300

    Update RecursivePrime.java

--- resources/src/week2/RecursivePrime.java
+++ resources/src/week2/RecursivePrime.java
@@ -3,27 +3,33 @@ package week2;
 import java.util.Scanner;
 
 public class RecursivePrime {
-    //Asal sayı kontrol eden method:
-    public static boolean isPrime(int n){
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0){
-                return false;
-            }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Kullanıcıdan sayıyı al:
+        System.out.print("Lütfen bir sayı girin: ");
+        int number = scanner.nextInt();
+
+        //Asal sayı metodunu çağırıp, gelen değere göre mesaj yazdır:
+        if (isPrime(number, number / 2)) {
+            System.out.println(number + " bir asal sayıdır.");
+        } else {
+            System.out.println(number + " bir asal sayı değildir.");
         }
-        return true;
+
+        scanner.close();
     }
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        //Kullancıdan alınan sayıyı isPrime methoduna yolla ve geriye gelen boolean değerini yeni lokal değişkene ata:
-        System.out.println("Bu program recursive method kullanarak asal sayı incelemesi yapar. \n Lütfen incelenecek sayıyı giriniz.");
-        int num = scanner.nextInt();
-        boolean isPrimeValue = isPrime(num);
-        //Değer doğru veya yanlış ise sonucu bastır:
-        String result = isPrimeValue ? "Sayı asal sayıdır." : "Sayı asal değildir.";
-        System.out.println(result);
+    // Sayının asal olduğunu belirlemek için recursive method:
+    public static boolean isPrime(int num, int divisor) {
+        if (divisor == 1) {
+            return true;
+        } else {
+            if (num % divisor == 0) {
+                return false;
+            } else {
+                return isPrime(num, divisor - 1);
+            }
+        }
     }
 }
 //Can Ekşioğlu
