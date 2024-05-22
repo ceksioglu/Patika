8352878e03d7f670ad69e6df6b23768e9a738733
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-22 16:12:54 +0300

    Update TryCatch2.java
    Patika W5

--- resources/src/week5/TryCatch2.java
+++ resources/src/week5/TryCatch2.java
@@ -7,6 +7,10 @@ public class TryCatch2 {
 
     static int[] arr = {2,4,6,8,10,12,14,16,18,20};
 
+    static void getIndexData(int n) {
+        System.out.println("Seçilen indeksteki değer :"+ arr[n]);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Dizi : " +Arrays.toString(arr));
@@ -14,7 +18,7 @@ public class TryCatch2 {
 
         try {
             int choice = scanner.nextInt();
-            System.out.println("Seçilen indeks değeri :"+ arr[choice]);
+            getIndexData(choice);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getLocalizedMessage());
         } finally {
