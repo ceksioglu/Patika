cf190d3fecdeb9a6388d556c417516c527c9857a
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-24 17:51:16 +0300

    Book.java added.
    Patika W5

---
+++ resources/src/week5/Book.java
@@ -0,0 +1,68 @@
+package week5;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Book implements Comparable<Book> {
+    private int pageCount;
+    private String title;
+
+    public Book(int pageCount, String title) {
+        this.pageCount = pageCount;
+        this.title = title;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        return this.title.compareTo(other.title);
+    }
+
+    @Override
+    public String toString() {
+        return "Book {" +
+                "Title='" + title + '\'' +
+                ", Page Count=" + pageCount +
+                '}';
+    }
+
+
+
+    public static void main(String[] args) {
+        Set<Book> bookSet = new TreeSet<>();
+        bookSet.add(new Book(500, "Java 101"));
+        bookSet.add(new Book(500, "Effective Java"));
+        bookSet.add(new Book(300, "Spring 101"));
+        bookSet.add(new Book(400, "Clean Code"));
+        bookSet.add(new Book(450, "Design Patterns"));
+
+
+        for (Book book : bookSet ){
+            System.out.println(book);
+        }
+
+        Set<Book> bookSetByPageCount = new TreeSet<>(Comparator.comparingInt(Book::getPageCount));
+        bookSetByPageCount.addAll(bookSet);
+        System.out.println("\nBooks sorted by page count:");
+        for (Book book : bookSetByPageCount) {
+            System.out.println(book);
+        }
+    }
+
+}
--- resources/src/week5/tryCatch1.java
+++ resources/src/week5/tryCatch1.java
@@ -1,5 +1,6 @@
 package week5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class tryCatch1 {
@@ -12,11 +13,21 @@ public class tryCatch1 {
         int a = 0;
         int b = 20;
         int c;
+        int[] arr = new int[5];
         try {
             System.out.println("Start block:");
+            c = scanner.nextInt();
+            arr[7]=10;
             System.out.println(b / a);
-        } catch (Exception e){
+        } catch (ArithmeticException e){
             System.out.println(e.getMessage()); //Error bastır. e = Exception'a veridiğin isim.
+        } catch (InputMismatchException e){
+            System.out.println("Wrong data type");
+            System.out.println(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array error;");
+        } finally {
+            System.out.println("Final");
         }
 
         System.out.println("End");
