6268f8480de6722812976e7a01a655098ce75a4c
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-24 18:52:39 +0300

    Update ProductManager.java

--- resources/src/week5/PatikaStore/services/ProductManager.java
+++ resources/src/week5/PatikaStore/services/ProductManager.java
@@ -10,6 +10,9 @@ import week5.PatikaStore.models.Cellphone;
 import week5.PatikaStore.models.Notebook;
 
 public class ProductManager {
-
+    private List<Brand> brands = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
+    private static int productId = 1;
     
+
 }
