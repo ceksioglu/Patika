45283224eabc3ba32b1f43643d00d59e2be4a54b
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-26 18:49:30 +0300

    Update WordFrequency.java

--- resources/src/week5/WordFrequency.java
+++ resources/src/week5/WordFrequency.java
@@ -1,32 +1,39 @@
-package week5;
-
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class WordFrequency {
-
     public static void main(String[] args) {
+        // Kullanıcıdan metin almak için Scanner nesnesi oluşturuluyor
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Lütfen bir metin girin :");
+        // Kullanıcıdan metin isteniyor
+        System.out.println("Please enter a text:");
         String inputText = scanner.nextLine();
 
-        String[] words = inputText.split("\\s+");
+        // Metni kelimelere ayırma (\\W+ ile)
+        String[] words = inputText.split("[\\W]+");
 
-        HashMap<String,Integer> wordCountMap = new HashMap<>();
+        // Kelime frekanslarını tutmak için bir HashMap oluşturuluyor
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
 
-        System.out.println("Girilen kelimeler :");
-        for(String splitWord : words){
-            System.out.print(splitWord + " ");
+        // Her bir kelimenin frekansını hesaplama
+        for (String splitWord : words) {
+            // Kelimeyi küçük harflere çevirerek (case-insensitive) hesaplarken birleştirme sağlıyoruz
             splitWord = splitWord.toLowerCase();
-            if (wordCountMap.containsKey(splitWord)){
-               // wordCountMap.put(splitWord.get)
-            }else {
-                wordCountMap.put(splitWord,1);
+            if (wordCountMap.containsKey(splitWord)) {
+                wordCountMap.put(splitWord, wordCountMap.get(splitWord) + 1);
+            } else {
+                wordCountMap.put(splitWord, 1);
             }
         }
 
+        // Kelime frekanslarını ekrana yazdırma
+        System.out.println("Word frequencies:");
+        for (String word : wordCountMap.keySet()) {
+            System.out.println(word + ": " + wordCountMap.get(word));
+        }
 
-
+        // Scanner nesnesini kapatma
+        scanner.close();
     }
 }
