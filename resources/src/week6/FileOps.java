72132b64446d7a9ddac2e60316cc0cef4dd5d51b
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-06-01 10:37:34 +0300

    -

--- resources/src/week6/FileOps.java
+++ resources/src/week6/FileOps.java
@@ -2,7 +2,7 @@ package week6;
 
 import java.util.Scanner;
 
-public class test {
+public class FileOps {
     public static void main(String[] args) {
         String word;
         Scanner scanner = new Scanner(System.in);
