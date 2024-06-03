b1557edea36886768c9774b107c098ed7a131d5b
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-06-03 20:24:02 +0300

    Create DBTransaction.java

---
+++ src/week6_db/DBTransaction.java
@@ -0,0 +1,11 @@
+package week6_db;
+
+public class DBTransaction {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/university";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "root";
+    
+    public static void main(String[] args) {
+
+    }
+}
